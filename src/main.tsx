36c7c631c07febb3bfda04a730969190ebf60e60
Yan <visualection@gmail.com>
2023-06-01 18:33:15 +0200

    Add routes

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,11 @@ import { fetchUsers } from "./features/users/usersSlice.ts";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UsersList from "./features/users/UsersList.tsx";
 import UserItem from "./features/users/UserItem.tsx";
+import { fetchAllPosts } from "./features/posts/postsSlice.ts";
+import PostPage from "./features/posts/PostPage.tsx";
 
 store.dispatch(fetchUsers());
+store.dispatch(fetchAllPosts());
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
@@ -20,6 +23,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           <Route path="/" element={<App />} />
           <Route path="/users" element={<UsersList />} />
           <Route path="/users/:id" element={<UserItem />} />
+          <Route path="/posts/:id" element={<PostPage />} />
         </Routes>
       </React.StrictMode>
     </BrowserRouter>
